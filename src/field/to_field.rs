32a2dce4f957c9432c18cabe44c9e6c7031a18fc
Sérgio Carvalho <sergio.carvalho@sooma.com>
2026-05-06 16:57:55 +0100

    Implement impl_to_field_for_numeric macro in field, and use it to generate ToField implementations

--- src/field/to_field.rs
+++ src/field/to_field.rs
@@ -13,114 +13,23 @@ impl ToField for bool {
         ))
     }
 }
-impl ToField for i8 {
-    fn to_field(&self) -> Field {
-        Field::Number(crate::field::Number::new(
-            "".to_string(),
-            "".to_string(),
-            *self as f64,
-        ))
-    }
+macro_rules! impl_to_field_for_numeric {
+    ($($t:ty),*) => {
+        $(
+            impl ToField for $t {
+                fn to_field(&self) -> Field {
+                    Field::Number(crate::field::Number::new(
+                        "".to_string(),
+                        "".to_string(),
+                        *self as f64,
+                    ))
+                }
+            }
+        )*
+    };
 }
-impl ToField for i16 {
-    fn to_field(&self) -> Field {
-        Field::Number(crate::field::Number::new(
-            "".to_string(),
-            "".to_string(),
-            *self as f64,
-        ))
-    }
-}
-impl ToField for i32 {
-    fn to_field(&self) -> Field {
-        Field::Number(crate::field::Number::new(
-            "".to_string(),
-            "".to_string(),
-            *self as f64,
-        ))
-    }
-}
-impl ToField for i64 {
-    fn to_field(&self) -> Field {
-        Field::Number(crate::field::Number::new(
-            "".to_string(),
-            "".to_string(),
-            *self as f64,
-        ))
-    }
-}
-impl ToField for u8 {
-    fn to_field(&self) -> Field {
-        Field::Number(crate::field::Number::new(
-            "".to_string(),
-            "".to_string(),
-            *self as f64,
-        ))
-    }
-}
-impl ToField for u16 {
-    fn to_field(&self) -> Field {
-        Field::Number(crate::field::Number::new(
-            "".to_string(),
-            "".to_string(),
-            *self as f64,
-        ))
-    }
-}
-impl ToField for u32 {
-    fn to_field(&self) -> Field {
-        Field::Number(crate::field::Number::new(
-            "".to_string(),
-            "".to_string(),
-            *self as f64,
-        ))
-    }
-}
-impl ToField for u64 {
-    fn to_field(&self) -> Field {
-        Field::Number(crate::field::Number::new(
-            "".to_string(),
-            "".to_string(),
-            *self as f64,
-        ))
-    }
-}
-impl ToField for f32 {
-    fn to_field(&self) -> Field {
-        Field::Number(crate::field::Number::new(
-            "".to_string(),
-            "".to_string(),
-            *self as f64,
-        ))
-    }
-}
-impl ToField for f64 {
-    fn to_field(&self) -> Field {
-        Field::Number(crate::field::Number::new(
-            "".to_string(),
-            "".to_string(),
-            *self,
-        ))
-    }
-}
-impl ToField for isize {
-    fn to_field(&self) -> Field {
-        Field::Number(crate::field::Number::new(
-            "".to_string(),
-            "".to_string(),
-            *self as f64,
-        ))
-    }
-}
-impl ToField for usize {
-    fn to_field(&self) -> Field {
-        Field::Number(crate::field::Number::new(
-            "".to_string(),
-            "".to_string(),
-            *self as f64,
-        ))
-    }
-}
+impl_to_field_for_numeric!(i8, i16, i32, i64, u8, u16, u32, u64, f32, f64, isize, usize);
+
 impl ToField for String {
     fn to_field(&self) -> Field {
         Field::Text(crate::field::Text::new(
