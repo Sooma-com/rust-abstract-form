50611bac5ba183bba230acca45a8164b7d45dd64
Sérgio Carvalho <sergio.carvalho@sooma.com>
2026-06-02 17:32:20 +0100

    Support hex_color::HexColor

--- src/fieldset/to_field_set.rs
+++ src/fieldset/to_field_set.rs
@@ -1,4 +1,5 @@
 use crate::FieldSet;
+use crate::field::concrete_single_value::ConcreteSingleValue;
 use crate::field::single_value::SingleValue;
 use crate::fieldset::to_empty_fieldset::ToEmptyFieldSet;
 use std::sync::Arc;
@@ -7,7 +8,7 @@ pub trait ToFieldSet {
     fn to_field_set(&self) -> FieldSet;
 }
 
-macro_rules! impl_to_field_set_for_primitive {
+macro_rules! impl_to_field_set_for_single_value {
     ($($t:ty),*) => {
         $(
             impl ToFieldSet for $t {
@@ -27,11 +28,11 @@ macro_rules! impl_to_field_set_for_primitive {
         )*
     };
 }
-impl_to_field_set_for_primitive!(
+impl_to_field_set_for_single_value!(
     String, bool, i8, i16, i32, i64, u8, u16, u32, u64, f32, f64, isize, usize
 );
 #[cfg(feature = "chrono")]
-impl_to_field_set_for_primitive!(chrono::NaiveDate, chrono::NaiveTime, chrono::NaiveDateTime);
+impl_to_field_set_for_single_value!(chrono::NaiveDate, chrono::NaiveTime, chrono::NaiveDateTime);
 
 impl<T> ToFieldSet for Option<T>
 where
@@ -44,3 +45,27 @@ where
         }
     }
 }
+
+macro_rules! impl_to_field_set_for_concrete_single_value {
+    ($($t:ty),*) => {
+        $(
+            impl ToFieldSet for $t {
+                fn to_field_set(&self) -> FieldSet {
+                    let mut result = FieldSet::new("".to_string(), "".to_string());
+                    result
+                        .controls
+                        .push(Arc::new(Box::new(ConcreteSingleValue::<$t> {
+                            tag: "".to_string(),
+                            label: "".to_string(),
+                            value: self.clone(),
+                            validations: vec![],
+                        })));
+                    result
+                }
+            }
+        )*
+    };
+}
+
+#[cfg(feature = "hex_color")]
+impl_to_field_set_for_concrete_single_value!(hex_color::HexColor);
